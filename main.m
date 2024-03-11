aba42bad557fe9d092bbb444f6416a87bcd4721a
Kurt Butler <62613188+newyorkfishmarket@users.noreply.github.com>
2024-03-11 14:57:11 -0400

    Create main.m

---
+++ main.m
@@ -0,0 +1,41 @@
+%% Explainable Learning with Gaussian Processes
+% This code reproduces the figures from our paper. To generate all figures 
+% (as .png files), you just need to run main.m. The code should run with no
+% issues using Matlab 2022a or later. All generated figures and tables will
+% be saved to the results folder.
+
+%% Set up the Matlab path
+addpath(genpath('./data'))
+addpath('./functions')
+addpath('./results')
+addpath('./scripts')
+
+%% Experiments
+% Running these scripts will reproduce experiments from the paper
+
+% Heteroscedasticity of the attribution GPs 
+% (Fig. 1 of the paper)
+exp3_heteroscedastic
+
+% Breast Cancer Prognostic data 
+% (Fig. 2 of the paper)
+exp1_general
+
+% Taipei housing data figure 
+% (Fig. 3 in the paper)
+exp2_taipei
+
+% Numerical integration comparison 
+% (Fig. 4 of the paper)
+exp4_quadrature
+
+% RFGP sparse approximation comparison 
+% (Fig. 5 of the paper)
+exp5_rfgps
+
+
+% Appendices
+% exp4b_converg
+% suppl_quadrature
+
+
